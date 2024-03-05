4367331df2a14c6a2de60dc64bad73b8ba9525f2
Dandrizzy <dandrizzy4all@gmail.com>
2024-03-06 00:32:27 +0100

    up

--- src/ui/Nav.jsx
+++ src/ui/Nav.jsx
@@ -5,31 +5,38 @@ import { useNavigate } from 'react-router-dom';
 import Logout from '../Features/authentication/Logout';
 import Logins from '../Features/authentication/Logins';
 import { useUser } from '../Features/authentication/useUser';
+import SpinnerMini from './SpinnerMini';
+import { DashboardIcon } from '@radix-ui/react-icons';
 
 
 const Nav = () => {
- const { isAuthenticated } = useUser();
+  const { isAuthenticated, user, isLoading } = useUser();
 
- const navigate = useNavigate();
+  const navigate = useNavigate();
+  if (isLoading) return <SpinnerMini />;
+  return (
+    <div className=" hidden items-center md:grid grid-cols-2 p-4 bg-blue-700 text-slate-100">
+      <img onClick={() => navigate('/')} src='/ve.png' alt='Logo' className=' h-12 bg-zinc-100 p-2 rounded-full' />
 
- return (
-  <div className=" hidden items-center md:grid grid-cols-2 p-4 bg-blue-700 text-slate-100">
-   <img onClick={() => navigate('/')} src='/ve.png' alt='Logo' className=' h-12 bg-zinc-100 p-2 rounded-full' />
+      <ul className=" lg:text-lg [&>*]:cursor-pointer flex justify-between">
+        <li className=' hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2 flex items-center justify-center gap-1' onClick={() => navigate('/')}><MdHome />Home</li>
 
-   <ul className=" lg:text-lg [&>*]:cursor-pointer flex justify-between">
-    <li className=' hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2 flex items-center justify-center gap-1' onClick={() => navigate('/')}><MdHome />Home</li>
+        <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2' onClick={() => scrollToSection('contact')}><MdMail />Contact</li>
 
-    <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2' onClick={() => scrollToSection('contact')}><MdMail />Contact</li>
+        <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2' ><FaLocationPin />Track</li>
 
-    <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2' ><FaLocationPin />Track</li>
+        {user?.id === 'd82350ef-5ec5-4768-a819-06d04b8b62b8' && <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full px-2 py-0' onClick={() => {
 
-    <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2' >
-     {isAuthenticated ? <Logout /> :
-      <Logins />}
-    </li>
-   </ul>
-  </div>
- );
+          navigate('/dashboard');
+        }}><DashboardIcon />Admin</li>}
+
+        <li className=' flex items-center justify-center gap-1 hover:bg-neutral-200/20 transition-all duration-300 rounded-full p-2' >
+          {isAuthenticated ? <Logout /> :
+            <Logins />}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Nav;
